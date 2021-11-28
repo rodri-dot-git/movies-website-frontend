6af344f57388faec34e5ef23cfc2c79b9e2207ea
rodri-dot-git <rodri230998@gmail.com>
2021-11-27 20:44:12 -0600

    header

--- src/views/App.tsx
+++ src/views/App.tsx
@@ -5,20 +5,8 @@ import '../css/App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <header className="App-header">🎥 Movies & TV Shows</header>
+      <div className="App-subheader">by: Rodrigo de León</div>
     </div>
   );
 }
